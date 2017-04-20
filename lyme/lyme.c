77b4d434525c26a6beef6124eb86b1cd6473f1e9
Anders Maraviglia <anders_Maraviglia@yahoo.com>
2017-04-20 13:58:57 -0400

    modifications..:

--- lyme/lyme.c
+++ lyme/lyme.c
@@ -2,14 +2,16 @@
 /* Includes ****************************************************************/
 /***************************************************************************/
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<errno.h>
-#include<math.h>
-#include<clcg4.h>
-#include<mpi.h>
-#include<pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <math.h>
+#include <clcg4.h>
+#include <mpi.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <dirent.h>
 
 /***************************************************************************/
 /* Defines *****************************************************************/
@@ -76,6 +78,7 @@ int carryAdult;			//numer of adult ticks a mice carries when it gets bit
 /***************************************************************************/
 void initUniverse();
 
+
 /***************************************************************************/
 /* Notes         ***********************************************************/
 /***************************************************************************/
@@ -114,7 +117,7 @@ int main(int argc, char* argv[])
    	days = 1;
    	infectionRate = 25;
    	numThreadsPer = 0;
-   	universeSize =  6;
+   	universeSize =  16;
    	miceTravelDays = 4;
    	tickFeedingDays = 3;
    	totalNumMouse = 9;
@@ -132,7 +135,7 @@ int main(int argc, char* argv[])
     }
 
 //Initialize universe with ticks, mice, and deer
-   initUniverse();
+   //initUniverse();
 
 // Start timing
     double start = 0;
@@ -146,14 +149,100 @@ int main(int argc, char* argv[])
    if (myRank == 0) end = MPI_Wtime();
    printf("Time taken: %lf s\n", end - start);
 
-//Frees
+  //Frees
 
+   MPI_Barrier( MPI_COMM_WORLD );
+   MPI_Finalize();
+   return 0;
+}
+
+/***************************************************************************/
+/* Other Functions  ********************************************************/
+/***************************************************************************/
+
+void read_input_file(char* filepath, char* entity){
+  MPI_File fp;
+  MPI_File_open(MPI_COMM_WORLD, filepath, MPI_MODE_RDWR, MPI_INFO_NULL, &fp);
+  // numRowsPer, myRank
+  int file_offset = numRowsPer*myRank;
+  for(int i = 0; i < numRowsPer; i++){
+    MPI_File_read_at_all(&fp, );
+  }
+  char line[universeSize*4]; // make sure our line is big enough
+  MPI_File_close(&fp);
+}
+
+void parse_input_files(char* dirpath){
+  DIR *dir;
+  struct dirent *ent;
+  if ((dir = opendir (dirpath)) != NULL) {
+    while ((ent = readdir (dir)) != NULL) {
+      if(strcmp(ent->d_name, "..") != 0 && strcmp(ent->d_name, ".") != 0){
+        char* underscore = strchr(ent->d_name, '_');
+        char* period = strchr(ent->d_name, '.');
+        int underscore_index = (int)(underscore - ent->d_name);
+        int period_index = (int)(period - ent->d_name);
+        if(underscore_index >= 0 && underscore_index < strlen(ent->d_name) && period_index > underscore_index){
+          char entity[period_index - underscore_index];
+          strncpy(entity, ent->d_name + underscore_index+1, period_index - underscore_index -1);
+          char filepath[strlen(dirpath)+strlen(ent->d_name)];
+          strncpy(filepath, dirpath, strlen(dirpath));
+          strncpy(filepath+strlen(dirpath), ent->d_name, strlen(ent->d_name));
+          read_input_file(filepath, entity);
+          //printf ("\tfilename[%s] entity filepath[%s]\n", ent->d_name, filepath);
+        } else{
+          perror("bad file name!");
+        }
+        
+      }
+    }
+    closedir (dir);
+  } else{
+    perror("failed to read file 2");
+  }
 }
 
 void initUniverse() { 
-	/*Comments: It's going to be hard to allocate a total number of mice across the entire
-	universe, due to it being across ranks. I was thinking we create a Python program to do it, output 
-	the allocations to a text file, and this program has each rank read its portion of allocations from the
-	text file. */
+  	/*Comments: It's going to be hard to allocate a total number of mice across the entire
+  	universe, due to it being across ranks. I was thinking we create a Python program to do it, output 
+  	the allocations to a text file, and this program has each rank read its portion of allocations from the
+  	text file. */
+    // dir '/boards'
+    // subdir '/16x16'
+  char cwd[1024];
+  DIR *dir;
+  struct dirent *ent;
+  if (getcwd(cwd, sizeof(cwd)) != NULL) // get current working dir
+    fprintf(stdout, "Current working dir: %s\n", cwd);
+  else
+    perror("getcwd() error");
+  strncpy(cwd+strlen(cwd), "/boards/", 8);
+  printf("new dir: %s\n", cwd);
+  if ((dir = opendir (cwd)) != NULL) { // find the correct input file directory
+    while ((ent = readdir (dir)) != NULL) {
+      if(strcmp(ent->d_name, "..") != 0 && strcmp(ent->d_name, ".") != 0){
+        char* x = strchr(ent->d_name, 'x');
+        int x_index = (int)(x - ent->d_name);
+        if(x_index >= 0 && x_index < strlen(ent->d_name)){
+          char dir_uni_size_str[x_index];
+          strncpy(dir_uni_size_str, ent->d_name, x_index);
+          int dir_uni_size = atoi(dir_uni_size_str);
+          if(dir_uni_size == universeSize){ // read in correct input files
+            char dirpath[strlen(cwd)+strlen(ent->d_name)+1];
+            strncpy(dirpath, cwd, strlen(cwd));
+            strncpy(dirpath+strlen(cwd), ent->d_name, strlen(ent->d_name));
+            dirpath[strlen(cwd)+strlen(ent->d_name)] = '/';
+            //printf ("dirname[%s], dirpath[%s]\n", ent->d_name, dirpath);
+            parse_input_files(dirpath);
+          }
+          
+        } else
+          printf("inproper universe dir[%s]\n", ent->d_name);
+      }
+    }
+    closedir (dir);
+  } else{
+    perror("failed to read file");
+  }
 
 }
